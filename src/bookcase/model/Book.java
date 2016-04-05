4d3465caa833efd37a9c4ef120c1c4ce84026de1
Jelle vd veen <jellevdveen16@gmail.com>
2016-04-05 16:45:03 +0200

    added user, book and bookcase classes + demo

--- src/bookcase/model/Book.java
+++ src/bookcase/model/Book.java
@@ -4,4 +4,28 @@ package bookcase.model;
  * Created by Kyra on 05/04/2016.
  */
 public class Book {
+    private long isbn;
+    private String title;
+    private String author;
+
+    public Book(long isbn, String title, String author) {
+        this.author = author;
+        this.isbn = isbn;
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object O) {
+        if (O instanceof Book) {
+            if (((Book) O).isbn == this.isbn) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return (this.title + " " + this.author);
+    }
 }
--- src/bookcase/model/User.java
+++ src/bookcase/model/User.java
@@ -1,7 +1,54 @@
 package bookcase.model;
 
+import java.util.ArrayList;
+
 /**
  * Created by Kyra on 05/04/2016.
  */
 public class User {
+    private ArrayList<Bookcase> bookcaseList = new ArrayList<Bookcase>();
+    private String name = "";
+
+    public User() {}
+
+    // getters
+    public String getName(){return name;}
+    public ArrayList<Bookcase> getBookcaseList(){return bookcaseList;}
+
+    // setters
+    public void loginUser(String name, String passHash){
+        boolean validLogin = (name.equals("Kyra") && passHash.equals("Jelle"));
+
+        if (validLogin) {
+            this.name = name;
+            retrieveBookcases();
+        }
+    }
+
+
+    public ArrayList<Bookcase> retrieveBookcases() {
+        if (this.name.equals("")){
+            return null;
+        } else {
+            // todo this.bookcaseList = ....import Bookcase-list from database
+            return this.bookcaseList;
+        }
+    }
+
+    public void addBookcase(String name) {
+        this.bookcaseList.add(new Bookcase(name));
+    }
+
+    public static void main(String[] args) {
+        User activeUser = new User();
+        activeUser.loginUser("Kyra", "Jelle");
+        activeUser.addBookcase("Nice Bookcase");
+
+        Bookcase activeCase = activeUser.getBookcaseList().get(0);
+        activeCase.addBook(new Book(1616161616161l, "TestBook", "TestAuthor"));
+
+        for (Book b : activeCase.getBookcaseBooks()) {
+            System.out.println(b);
+        }
+    }
 }
