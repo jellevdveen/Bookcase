c2a41ebec6c40c7f29ecfbe2ce5b2d606eca9639
Kyra <kyratijhuis@outlook.com>
2016-04-06 21:08:42 +0200

    - New interface with database constants - updated database initialization file - fixed database connection in DB.java

--- mysql/initDatabase.sql
+++ mysql/initDatabase.sql
@@ -1,27 +1,29 @@
-drop table if exists user;
-drop table if exists book;
-drop table if exists bookcase;
 drop table if exists book_in_bookcase;
+drop table if exists bookcase;
+drop table if exists book;
+drop table if exists user;
+
 
 create table user
 (
 	user_id			int auto_increment,
     password_hash	varchar(40) not null,
     user_name		varchar(20) not null,
-    primary key (user_id)
+    primary key (user_id),
+    unique (user_name)
 );
 
 create table book
 (
 	isbn			varchar(13) not null,
-    title			varchar(80) not null,
+    book_title		varchar(80) not null,
     author			varchar(60) not null,
     primary key (isbn)
 );    
 
 create table bookcase
 (
-	title			varchar(60) not null,
+	bookcase_name	varchar(60) not null,
     user_id			int not null,
     bookcase_id		int auto_increment,
     primary key (bookcase_id),
@@ -36,3 +38,8 @@ create table book_in_bookcase
     foreign key (isbn) references book(isbn) on delete cascade,
     foreign key (bookcase_id) references bookcase(bookcase_id) on delete cascade
 );
+
+insert into user (password_hash, user_name) values ('password', 'user');
+insert into book values ('1234567890123', 'Over het water', 'Van den Brink');
+insert into bookcase (bookcase_name, user_id) values ('kast',1);
+insert into book_in_bookcase values ('1234567890123',1);
--- src/database/DB.java
+++ src/database/DB.java
@@ -1,7 +1,57 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Created by Kyra on 05/04/2016.
  */
 public class DB {
+
+    private Connection connection = null;
+    private Statement statement = null;
+    private PreparedStatement prepStatement = null;
+    private ResultSet resultSet = null;
+
+    public void readDataBase() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3456/bookcase?" +
+                            "user=bookcase&password=Ballerup2750!");
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("select * from user");
+
+            writeResultSet(resultSet);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeResultSet(ResultSet resultSet) throws SQLException {
+        // ResultSet is initially before the first data set
+        while (resultSet.next()) {
+            // It is possible to get the columns via name
+            // also possible to get the columns via the column number
+            // which starts at 1
+            // e.g. resultSet.getSTring(2);
+            System.out.println(resultSet.getString(2));
+        }
+    }
+
+    public static void main(String[] args) {
+        DB obj = new DB();
+        obj.readDataBase();
+    }
 }
